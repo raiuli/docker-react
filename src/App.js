378cf9f853929f6edcb5460ea2b41c2aba22ccb8
raiuli <raihan.ul.isalm@ltu.se>
2022-02-15 21:08:26 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with raihan with docker compose
         </a>
       </header>
     </div>
